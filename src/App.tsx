dd9b5a4a751b448cd333c189d59e89ccbf1a6968
Brennon Gabriel de Oliveira <brennonoliveira20014@gmail.com>
2023-09-27 02:28:03 -0300

    Sessão ToolBar

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,12 @@ import 'path-browserify';
 import { BsArrowReturnLeft, BsArrowReturnRight } from 'react-icons/bs';
 import { AiFillAlert, AiFillAmazonCircle } from 'react-icons/ai';
 import ToolBar from './components/tools-bar';
+import ToolBarSection from './section/tools-bar/ToolBarSection';
 
 function App() {
 	return (
 		<div className="App">
-			<button onClick={() => process.exit()}>aaa</button>
-			<ToolBar.Root>
-				<p>teste</p>
-				<ToolBar.Divisor />
-				<ToolBar.Section>
-					<ToolBar.Button>
-						<BsArrowReturnLeft />
-					</ToolBar.Button>
-					<ToolBar.Button>
-						<BsArrowReturnRight />
-					</ToolBar.Button>
-				</ToolBar.Section>
-				<ToolBar.Divisor />
-				<ToolBar.Section>
-					<ToolBar.Button>
-						<AiFillAlert />
-					</ToolBar.Button>
-					<ToolBar.Button>
-						<AiFillAmazonCircle />
-					</ToolBar.Button>
-				</ToolBar.Section>
-			</ToolBar.Root>
+			<ToolBarSection />
 		</div>
 	);
 }
---
+++ src/section/tools-bar/ToolBarSection.tsx
@@ -0,0 +1,29 @@
+import { AiFillAlert, AiFillAmazonCircle } from 'react-icons/ai';
+import { BsArrowReturnLeft, BsArrowReturnRight } from 'react-icons/bs';
+import ToolBar from 'src/components/tools-bar';
+
+export default function ToolBarSection() {
+	return (
+		<ToolBar.Root>
+			<p>teste</p>
+			<ToolBar.Divisor />
+			<ToolBar.Section>
+				<ToolBar.Button>
+					<BsArrowReturnLeft />
+				</ToolBar.Button>
+				<ToolBar.Button>
+					<BsArrowReturnRight />
+				</ToolBar.Button>
+			</ToolBar.Section>
+			<ToolBar.Divisor />
+			<ToolBar.Section>
+				<ToolBar.Button>
+					<AiFillAlert />
+				</ToolBar.Button>
+				<ToolBar.Button>
+					<AiFillAmazonCircle />
+				</ToolBar.Button>
+			</ToolBar.Section>
+		</ToolBar.Root>
+	);
+}
